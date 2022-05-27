c8e75d660dea064b98e1dc3b315228e5367c888b
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2022-05-27 08:53:07 +0200

    PP-3: Add basic styles to ScreenOne component

--- components/screens/screen-one/ScreenOne.tsx
+++ components/screens/screen-one/ScreenOne.tsx
@@ -17,12 +17,16 @@ function ScreenOne() {
       <div className={styles.screenOne__container}>
 
         <div className={styles.mainBlock}>
-          <h1 className={styles.mainBlock__title}>Hey there, It's Pavlo Ivashchenko</h1>
+          <h1 className={styles.mainBlock__title}>Hey there, <br className="d-none d-md-inline-flex" />It's Pavlo Ivashchenko</h1>
           <h4 className={styles.mainBlock__subtitle}>Thanks for being there and reading my gig details. As the title of the gig suggests I am a Front end Developer & offering my expertise in Angular App Website Development.</h4>
           <div className={styles.mainBlock__buttonWrap}>
             <ButtonPrimary title="Learn more" link="/" filled={true}/>
           </div>
         </div>
+
+        <div className={styles.screenOne__decor_1}></div>
+        <div className={styles.screenOne__decor_2}></div>
+        <div className={styles.screenOne__decor_3}></div>
 
       </div>
     </section>
