1d1e72cba8b54a7e9d7860c7aec2440b4e54e1f8
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2022-06-17 22:11:13 +0200

    PP-7: Started work on footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,16 +1,36 @@
-import React, { useState, useEffect } from "react";
-import "@components/footer/Footer.module.scss";
+import React from "react";
+import { InView } from "react-intersection-observer";
+import { useTranslations } from "next-intl";
+
+import styles from "@components/footer/Footer.module.scss";
+
+
+const socialLinks = [
+  { iconName: 'facebook', link: '/' },
+  { iconName: 'instagram', link: '/' },
+  { iconName: 'linkedin', link: '/' },
+];
+
+const acchorLinks = [
+  { uiName: 'about', scrollTo: '' },
+  { uiName: 'resume', scrollTo: '' },
+  { uiName: 'porfolio', scrollTo: '' }
+];
 
 function Footer() {
-  const [state, setState] = useState(false);
+  const t = useTranslations('screen-three');
 
-  useEffect(() => {
-    return () => {}
-  }, []);
+  return (
+    <InView threshold={0.25}>
+      {({ref, inView}) => (
+        <section ref={ref} className={styles.screenThree}>
 
-  return <section>
-    footer
-  </section>;
+          
+
+        </section>
+      )}
+    </InView>
+  );
 }
 
 export default Footer;
