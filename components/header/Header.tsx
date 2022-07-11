c3bf70e1b18510c875a26448af7f11ebb122d2b6
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2022-07-11 19:16:01 +0200

    PP-9: Add imperative scroll to Header component

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect, useRef, RefObject, memo } from 'react';
-import Link from 'next/link';
 import { useTranslations } from 'next-intl';
 import { fromEvent, Subject } from 'rxjs';
 import { distinctUntilChanged, map, takeUntil, tap } from 'rxjs/operators';
 
+import { CONSTANTS } from '@root/constants';
 import Burger from '@components/burger/Burger';
 import LanguageBar from '@components/language-bar/LanguageBar';
 import ButtonPrimary from '@components/button-primary/ButtonPrimary';
@@ -12,10 +12,10 @@ import styles from '@components/header/Header.module.scss';
 
 
 const navLinks = [
-  { uiName: 'about' },
-  { uiName: 'services' },
-  { uiName: 'resume' },
-  { uiName: 'portfolio' }
+  { uiName: 'about', scrollToId: CONSTANTS.sectionIds.sectionTwo },
+  { uiName: 'services', scrollToId: CONSTANTS.sectionIds.sectionThree },
+  { uiName: 'resume', scrollToId: CONSTANTS.sectionIds.sectionFour },
+  { uiName: 'portfolio', scrollToId: CONSTANTS.sectionIds.sectionFive }
 ];
 
 function Header(props: { scrollOrigin: RefObject<HTMLDivElement>; }) {
@@ -49,32 +49,34 @@ function Header(props: { scrollOrigin: RefObject<HTMLDivElement>; }) {
     setBurgerExpandedState(!burgerExpandedState);
   }
 
+  const onClickAnchorHandler = (scrollToId: string) => {
+    document.getElementById(scrollToId)?.scrollIntoView();
+  }
+
   return (
     <div className={`${styles.header} ${isShrinked ? styles.header_isShrinked : ''}`}>
 
       <div className={styles.header__container}>
 
-        <Link href="/">
-          <p className={styles.header__logo}>
-            <span>Pavlo</span>
-            <span>Ivashchenko</span>
-          </p>
-        </Link>
+        <a onClick={() => onClickAnchorHandler(CONSTANTS.sectionIds.sectionOne)} className={styles.header__logo}>
+          <span>Pavlo</span>
+          <span>Ivashchenko</span>
+        </a>
 
         <ul className={`${styles.navDesktop} d-none d-md-flex`}>
           {navLinks.map(link => <li key={link.uiName} className={styles.navDesktop__item}>
-            <a className={styles.navDesktop__link}>{t(link.uiName)}</a>
+            <a onClick={() => onClickAnchorHandler(link.scrollToId)} className={styles.navDesktop__link}>{t(link.uiName)}</a>
           </li>)}
         </ul>
 
-        <div className="d-flex align-items-center">
-          <div className="mx-3 mx-md-2 d-flex align-center">
+        <div className='d-flex align-items-center'>
+          <div className='mx-3 mx-md-2 d-flex align-center'>
             <LanguageBar />
           </div>
           <div className={styles.header__btnDesktopWrap}>
-            <ButtonPrimary title={t("contact-btn")} link="/" iconName="link-arrow-green" iconNameHover="link-arrow-black" />
+            <ButtonPrimary onClick={() => onClickAnchorHandler(CONSTANTS.sectionIds.coreFooter)} title={t('contact-btn')} link='/' iconName='link-arrow-green' iconNameHover='link-arrow-black' />
           </div>
-          <div className="d-flex d-md-none">
+          <div className='d-flex d-md-none'>
             <Burger menuRef={menuRef} burgerToggle={burgerToggleHandler} />
           </div>
         </div>
@@ -87,7 +89,7 @@ function Header(props: { scrollOrigin: RefObject<HTMLDivElement>; }) {
               </li>)}
             </ul>
             <div className={styles.header__btnMobileWrap}>
-              <ButtonPrimary title={t("contact-btn")} link="/" iconName="link-arrow-green" iconNameHover="link-arrow-black" />
+              <ButtonPrimary onClick={() => onClickAnchorHandler(CONSTANTS.sectionIds.coreFooter)} title={t('contact-btn')} iconName='link-arrow-green' iconNameHover='link-arrow-black' />
             </div>
           </div>
         </div>
