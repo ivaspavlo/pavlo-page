49ce1652b76bd79eb6c01c194b6c8860a4ba9452
pavlo.ivashchenko <pavlo.ivashchenko@rbinternational.com>
2026-07-09 14:09:29 +0200

    PP-23: Add German language support

--- constants/index.ts
+++ constants/index.ts
@@ -1,26 +1,26 @@
 export const CONSTANTS = {
-  lang: ['en', 'ua'],
+  lang: ["en", "ua", "de"],
   sectionIds: {
-    scrollOrigin: 'scrollOrigin',
-    sectionOne: 'sectionOneId',
-    sectionTwo: 'sectionTwoId',
-    sectionThree: 'sectionThreeId',
-    sectionFour: 'sectionFour',
-    experience: 'experienceId',
-    portfolio: 'portfolioId',
-    coreFooter: 'coreFooterId'
+    scrollOrigin: "scrollOrigin",
+    sectionOne: "sectionOneId",
+    sectionTwo: "sectionTwoId",
+    sectionThree: "sectionThreeId",
+    sectionFour: "sectionFour",
+    experience: "experienceId",
+    portfolio: "portfolioId",
+    coreFooter: "coreFooterId",
   },
   coreMessages: {
-    supportUkraine: 'support-ukraine',
-    emailSentSuccess: 'email-sent-success',
-    emailSentFail: 'email-sent-fail'
+    supportUkraine: "support-ukraine",
+    emailSentSuccess: "email-sent-success",
+    emailSentFail: "email-sent-fail",
   },
   errorMessages: {
-    email: 'error-email',
-    minChar: 'error-min-char'
+    email: "error-email",
+    minChar: "error-min-char",
   },
   api: {
-    base: '/api',
-    contact: 'contact'
-  }
+    base: "/api",
+    contact: "contact",
+  },
 };
